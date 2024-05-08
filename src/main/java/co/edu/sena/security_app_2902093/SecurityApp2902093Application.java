61591fcae3ee7e60dc08338b933e099778c2564f
Wsalass <160495285+Wsalass@users.noreply.github.com>
2024-05-08 14:38:09 -0500

    CAMBIOS REALIZADOS

---
+++ src/main/java/co/edu/sena/security_app_2902093/SecurityApp2902093Application.java
@@ -0,0 +1,19 @@
+package co.edu.sena.security_app_2902093;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+import java.util.Set;
+
+@SpringBootApplication
+public class SecurityApp2902093Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApp2902093Application.class, args);
+	}
+
+}
